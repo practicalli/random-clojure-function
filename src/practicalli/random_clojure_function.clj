32a56904bbf1aceda911b5136a939cfd51d6228a
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-07-04 10:33:24 +0100

    New project using Clojure deps.edn
    ```
    clojure -A:new app practicalli/random-clojure-function
    ```

---
+++ src/practicalli/random_clojure_function.clj
@@ -0,0 +1,7 @@
+(ns practicalli.random-clojure-function
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/practicalli/random_clojure_function_test.clj
@@ -0,0 +1,7 @@
+(ns practicalli.random-clojure-function-test
+  (:require [clojure.test :refer :all]
+            [practicalli.random-clojure-function :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
