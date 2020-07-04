1d01f3f8745641fc47df62613f32b5c34a3bd84d
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-07-04 11:35:18 +0100

    Show functions from specific namespaces if pass as argument

--- src/practicalli/random_clojure_function.clj
+++ src/practicalli/random_clojure_function.clj
@@ -2,22 +2,41 @@
   (:gen-class))
 
 
+;; data
+
 (def standard-library-functions
   "Fully qualified function names from clojure.core"
   (vals (ns-publics 'clojure.core)))
 
+(def available-namespaces
+  "Fully qualified function names from available"
+  (mapcat #(vals (ns-publics %)) (all-ns)))
+
+;; helper functions
+
+(defn function-list
+  [namespace]
+  (vals (ns-publics namespace)))
+
+
+;; logic
+
 (defn random-function
   [function-list]
   (let [function-details (meta (rand-nth function-list))]
-    (str (function-details :name)
-         "\n  " (function-details :arglists  )
-         "\n  " (function-details :doc) )))
+    (str (function-details :ns) "/" (function-details :name)
+         "\n  " (function-details :arglists)
+         "\n  " (function-details :doc))))
+
 
 
 (defn -main
-  "Return a function name from the Clojure Standard library"
+  "Return a random function and its details
+  from the available namespaces"
   [& args]
-  (println (random-function standard-library-functions)))
+  (if (seq args)
+    (println (random-function (mapcat #(function-list (symbol %)) args)))
+    (println (random-function available-namespaces))))
 
 
 ;; REPL experiments
